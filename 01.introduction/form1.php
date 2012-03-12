a5742a51577edcc1ef20e693a150d6d05e87016b
RICHARD Eddy <richard.eddy@live.fr>
2012-03-12 15:27:06 +0100

    Select villes.

--- 01.introduction/form1.php
+++ 01.introduction/form1.php
@@ -2,20 +2,20 @@
 if (isset($_POST['firstname'])) {
     $prenom=$_POST['firstname'];
    
-    $link = mysqli_connect('localhost', 'root', 'alonso', 'formation'); //mysql_connect('[host]', '[user]', '[password]', '[database]');
+    $link = mysqli_connect('localhost', 'root', 'alonso', 'formation'); //mysqli_connect('[host]', '[user]', '[password]', '[database]');
     mysqli_query($link, 'SET NAMES UTF8');
     
     $sql="INSERT INTO contact (prenom) VALUES('$prenom')";
     $succes = mysqli_query($link, $sql);
    
     if($succes){
-        $msg="Bravo vous êtes inscrit.";
+        $msg="<font color='green'>Bravo vous êtes inscrit.</font>";
     } else {
-        $msg="Erreur pendant l'inscription";
+        $msg="<font color='orange'>Erreur pendant l'inscription.</font>";
     }
     mysqli_close($link);
 } else {
-    $msg ='Bonjour Inconnu';
+    $msg ="<font color='red'>Bonjour Inconnu.</font>";
 }
 
 ?>
@@ -26,6 +26,7 @@ if (isset($_POST['firstname'])) {
         <meta charset="UTF-8" />
     </head>
     <body>
+        <p><?php echo $msg; ?></p>
         <form action="#" method="post">
             <label for="firstname">Prénom :</label><input type="text" name="firstname" /><br/>
             <input type="submit" value="Envoyer!" />
