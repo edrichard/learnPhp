7b1aa878db8fd4153092095411a48bd6b60da79a
RICHARD Eddy <richard.eddy@live.fr>
2012-03-14 09:17:02 +0100

    Modify : 04.Uplodify

--- 04.Uplodify/index.php
+++ 04.Uplodify/index.php
@@ -3,19 +3,22 @@
     <head>
         <meta charset="UTF-8">
         <title>Test uploadify</title>
+        <link type="text/css" rel="stylesheet" href="uploadify/uplodify.css" />
         <script type="text/javascript" src="uploadify/jquery-1.4.2.min.js"></script>
         <script type="text/javascript" src="uploadify/swfobject.js"></script>
         <script type="text/javascript" src="uploadify/jquery.uploadify.v2.1.4.min.js"></script>
         <script>
             // <![CDATA[
             $(document).ready(function() {
-            $('#file_upload').uploadify({
-                'uploader'  : 'uploadify/uploadify.swf',
-                'script'    : 'uploadify/uploadify.php',
-                'cancelImg' : 'uploadify/cancel.png',
-                'folder'    : 'uploads/',
-                'auto'      : true
-            });
+                $('#file_upload').uploadify({
+                    'uploader'  : 'uploadify/uploadify.swf',
+                    'script'    : 'uploadify/uploadify.php',
+                    'cancelImg' : 'uploadify/cancel.png',
+                    'folder'    : 'uploads/',
+                    'buttonText':  'Parcourir',
+                    'multi'     : true,
+                    'auto'      : false
+                });
             });
             // ]]>
         </script>
@@ -24,6 +27,7 @@
         <h3>Upload de fichier :</h3>
         <p>Sélectionnez votre fichier puis cliquer sur le bouton télécharger.</p>
         <input id="file_upload" type="file" name="file_upload" />
-        <a href="javascript:$('#file_upload').uploadifyUpload();">Télécharger le(s) fichier(s)</a>
+        <a href="javascript:$('#uploadify').uploadifyUpload();">Télécharger le(s) photo(s)</a> |
+        <a href="javascript:jQuery('#uploadify').uploadifyClearQueue()">Effacer la liste de téléchargement</a>
     </body>
 </html>
