7468c4b9dd0bc37b3c3d58f018fd25e1a94173ec
RICHARD Eddy <richard.eddy@live.fr>
2012-03-14 18:51:28 +0100

    Modify projet uplodify

---
+++ 04.Uplodify/connexion.php
@@ -0,0 +1,3 @@
+<?php
+    $link = mysqli_connect('localhost', 'root', 'alonso', 'test'); //mysqli_connect('[host]', '[user]', '[password]', '[database]');
+    mysqli_query($link, 'SET NAMES UTF8');
--- 04.Uplodify/uploadify/uploadify.php
+++ 04.Uplodify/uploadify/uploadify.php
@@ -31,18 +31,10 @@ if (!empty($_FILES)) {
 	$targetPath = $_SERVER['DOCUMENT_ROOT'] . $_REQUEST['folder'] . '/';
 	$targetFile =  str_replace('//','/',$targetPath) . $_FILES['Filedata']['name'];
 	
-	// $fileTypes  = str_replace('*.','',$_REQUEST['fileext']);
-	// $fileTypes  = str_replace(';','|',$fileTypes);
-	// $typesArray = split('\|',$fileTypes);
-	// $fileParts  = pathinfo($_FILES['Filedata']['name']);
-	
-	// if (in_array($fileParts['extension'],$typesArray)) {
-		// Uncomment the following line if you want to make the directory if it doesn't exist
-		// mkdir(str_replace('//','/',$targetPath), 0755, true);
-		
-		move_uploaded_file($tempFile,$targetFile);
-		echo str_replace($_SERVER['DOCUMENT_ROOT'],'',$targetFile);
-	// } else {
-	// 	echo 'Invalid file type.';
-	// }
+        $targetFile2 =  $_FILES['Filedata']['name'];
+        $sql="INSERT INTO fichiers (fichier) VALUES('$targetFile2')";
+        $succes = mysqli_query($link, $sql);
+        
+	move_uploaded_file($tempFile,$targetFile);
+	echo str_replace($_SERVER['DOCUMENT_ROOT'],'',$targetFile);
 }
