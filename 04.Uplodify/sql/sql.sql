80f12737d8b8456aefdef2ec0361ed1ca841b260
RICHARD Eddy <richard.eddy@live.fr>
2012-03-17 10:54:52 +0100

    New dump SQL.

--- 04.Uplodify/sql/sql.sql
+++ 04.Uplodify/sql/sql.sql
@@ -5,6 +5,8 @@
 CREATE TABLE IF NOT EXISTS `fichiers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `fichier` varchar(255) NOT NULL,
+  `hauteur` int(11) NOT NULL,
+  `largeur` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=0 ;
