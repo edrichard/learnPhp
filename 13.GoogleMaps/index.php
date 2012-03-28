69f94462321874bc6914fabeac1731a2e429d4ea
RICHARD Eddy <richard.eddy@live.fr>
2012-03-28 23:44:15 +0200

    13.GoogleMaps

---
+++ 13.GoogleMaps/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Google Maps</title>
+    </head>
+    <body>
+        <div  style="float: left; margin-right: 15px">
+            <h3>Street</h3>
+            <!-- Google Maps Element Code -->
+            <iframe frameborder=0 marginwidth=0 marginheight=0 border=0 
+                    style="border:0;margin:0;width:300px;height:250px;" 
+                    src="http://www.google.com/uds/modules/elements/mapselement/iframe.html?maptype=roadmap&latlng=48.866606%2C2.309071&mlatlng=48.866606%2C2.309071&maddress1=8%20Rue%20Jean%20Goujon&maddress2=75008%20Paris%2C%20France&zoom=17&mtitle=La%20Maison%20Champs%20Elys%C3%A9es&element=true" 
+                    scrolling="no" allowtransparency="true">
+            </iframe>
+        </div>
+        <div  style="float: left; margin-right: 15px">
+            <h3>Hybrid</h3>
+            <!-- Google Maps Element Code -->
+            <iframe frameborder=0 marginwidth=0 marginheight=0 border=0
+                    style="border:0;margin:0;width:300px;height:250px;" 
+                    src="http://www.google.com/uds/modules/elements/mapselement/iframe.html?maptype=hybrid&latlng=48.866606%2C2.309071&mlatlng=48.866606%2C2.309071&maddress1=8%20Rue%20Jean%20Goujon&maddress2=75008%20Paris%2C%20France&zoom=17&mtitle=La%20Maison%20Champs%20Elys%C3%A9es&element=true" 
+                    scrolling="no" allowtransparency="true">
+            </iframe>
+        </div>
+        <div  style="float: left; margin-right: 15px">
+            <h3>Satellite</h3>
+            <!-- Google Maps Element Code -->
+            <iframe frameborder=0 marginwidth=0 marginheight=0 border=0 
+                    style="border:0;margin:0;width:300px;height:250px;" 
+                    src="http://www.google.com/uds/modules/elements/mapselement/iframe.html?maptype=satellite&latlng=48.866606%2C2.309071&mlatlng=48.866606%2C2.309071&maddress1=8%20Rue%20Jean%20Goujon&maddress2=75008%20Paris%2C%20France&zoom=17&mtitle=La%20Maison%20Champs%20Elys%C3%A9es&element=true" 
+                     scrolling="no" allowtransparency="true">
+           </iframe>
+        </div>
+        <div  style="float: left; margin-right: 15px">
+            <h3>Terrain</h3>
+            <!-- Google Maps Element Code -->
+            <iframe frameborder=0 marginwidth=0 marginheight=0 border=0 
+                    style="border:0;margin:0;width:300px;height:250px;" 
+                    src="http://www.google.com/uds/modules/elements/mapselement/iframe.html?maptype=terrain&latlng=48.866606%2C2.309071&mlatlng=48.866606%2C2.309071&maddress1=8%20Rue%20Jean%20Goujon&maddress2=75008%20Paris%2C%20France&zoom=17&mtitle=La%20Maison%20Champs%20Elys%C3%A9es&element=true" 
+                    scrolling="no" allowtransparency="true">
+            </iframe>
+        </div>
+        <p style="clear: both;">
+            Source : 
+            <a href="http://www.google.com/webelements/#!/maps" target="_blank">
+                http://www.google.com/webelements/#!/maps
+            </a>
+        </p>
+    </body>
+</html>
