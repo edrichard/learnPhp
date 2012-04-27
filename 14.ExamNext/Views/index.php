ec0134512adcefe9bd5ee6d060e11fb3bb34a94a
RICHARD Eddy <richard.eddy@live.fr>
2012-04-27 08:25:34 +0200

    thirdt  commit for 14.ExamNext : formating the code in page index.php view

--- 14.ExamNext/Views/index.php
+++ 14.ExamNext/Views/index.php
@@ -1,11 +1,22 @@
 <?php ob_start() ?>
-<h1><?php echo $title; ?></h1>
-<?php foreach ($recettes AS $recette) {?>
-<h3><a href="recette.php?id=<?php echo $recette['idRecette'] ;?>"><?php echo htmlentities($recette['titre']) ;?></a></h3>
-            <img src="Images/<?php echo $recette['nom'] ;?>" />
-            
- <?php } ?>
 
+<h1><?php echo $title; ?></h1>
+
+<?php foreach ($recettes AS $recette) {?>
+    <div class="">
+        <h3>
+            <a href="recette.php?id=<?php echo $recette['idRecette'] ;?>">
+                <?php echo htmlentities($recette['titre']) ;?>
+            </a>
+        </h3>
+    </div>
+   
+    
+    <div class="">
+        <img src="Images/<?php echo $recette['nom'] ;?>" width="200" />
+    </div>
+
+ <?php } ?>
 
 <?php $content = ob_get_clean() ?>
 
