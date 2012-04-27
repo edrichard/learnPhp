a621d0edef358d0c18aac661f51d7e96cabdb8fd
RICHARD Eddy <richard.eddy@live.fr>
2012-04-27 10:30:07 +0200

    modify the template index.php

--- 14.ExamNext/Views/index.php
+++ 14.ExamNext/Views/index.php
@@ -3,20 +3,24 @@
 <h1><?php echo $title; ?></h1>
 
 <?php foreach ($recettes AS $recette) {?>
-    <div class="">
+<div class="recette">
+
+    <div class="recetteTitre">
         <h3>
             <a href="recette.php?id=<?php echo $recette['idRecette'] ;?>">
                 <?php echo htmlentities($recette['titre']) ;?>
             </a>
         </h3>
     </div>
-   
-    
-    <div class="">
-        <img src="Images/<?php echo $recette['nom'] ;?>" width="200" />
+
+    <div class="recettePhoto">
+        <img src="Images/<?php echo $recette['nom'] ;?>" width="200" height="200" />
     </div>
 
- <?php } ?>
+    <div class="separator"></div>
+    
+</div>
+<?php } ?>
 
 <?php $content = ob_get_clean() ?>
 
